a3042106fd316098ea8cdc43705da304a1ea2e17
Andrew Jernigan <andrewjernigan@Andrews-MBP.fios-router.home>
2020-05-20 10:17:38 -0400

    react router basics

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import Nav from '../nav/nav'
  
 const App = () => (
   <div>
     <h1>Daytona Gang Race Pool!!!</h1>
+    <Router>
+        <Nav />
+    </Router>
   </div>
 );
  
