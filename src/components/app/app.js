3dd9356850a42aae7e3613c80a1118b07f431f61
Andrew Jernigan <andrewjernigan@Andrews-MBP.fios-router.home>
2020-05-21 10:36:47 -0400

    signup form with firebase done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 import Nav from "../Nav/nav";
 import Signup from "../SignUp/signup";
 import SignIn from "../SignIn/signin";
+import SignOut from "../SignOut/signout";
 import Home from "../Home/home";
+import Landing from "../Landing/landing";
+import PassChange from  "../PasswordChange/passwordchange";
+import PassForget from "../PasswordForget/passwordforget";
 import Account from "../Account/account";
 import Admin from "../Admin/admin";
+
 import Rules from "../Rules/rules";
 import Grid from "../StartGrid/grid";
 import LeaderBoard from "../LeaderBoard/LeaderBoard";
@@ -23,8 +29,12 @@ const App = () => (
         <Nav />
         <hr />
         <Route exact path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.LANDING} component={Landing} />
         <Route path={ROUTES.SIGN_UP} component={Signup} />
         <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.SIGN_OUT} component={SignOut} />
+        <Route path={ROUTES.PASS_CHANGE} component={PassChange} />
+        <Route path={ROUTES.PASS_FORGET} component={PassForget} />
         <Route path={ROUTES.ACCOUNT} component={Account} />
         <Route path={ROUTES.ADMIN} component={Admin} />
 
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 import * as ROUTES from "../../constants/routes";
 
@@ -41,6 +43,30 @@ const Nav = () => (
       </ul>
     </div>
   </div>
+  // <div>
+  //   <Nav fill variant="tabs" defaultActiveKey="/home">
+  //     <Nav.Item>
+  //       <Nav.Link href="/home">
+  //         <Link to={ROUTES.RULES}>Rules</Link>
+  //       </Nav.Link>
+  //     </Nav.Item>
+  //     <Nav.Item>
+  //       <Nav.Link eventKey="link-1">
+  //       <Link to={ROUTES.GRID}>Drivers</Link>
+  //       </Nav.Link>
+  //     </Nav.Item>
+  //     <Nav.Item>
+  //       <Nav.Link eventKey="link-2">
+  //         <Link to={ROUTES.LEADERBOARD}>Leader Board</Link>
+  //       </Nav.Link>
+  //     </Nav.Item>
+  //     <Nav.Item>
+  //       <Nav.Link eventKey="disabled" disabled>
+  //         <Link to={ROUTES.DRAFT}>Draft</Link>
+  //       </Nav.Link>
+  //     </Nav.Item>
+  //    </Nav>
+  // </div>
 );
 
 export default Nav;
