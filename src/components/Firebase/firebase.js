a9171b3c252cba1bb12b0a78a11b29cce077bab3
Andrew Jernigan <andrewjernigan@MacBook-Pro.fios-router.home>
2020-05-29 12:45:28 -0400

    firebase file

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,41 @@
+import app from "firebase/app";
+import "firebase/auth";
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: ,
+  authDomain: "daytona-gang.firebaseapp.com",
+  databaseURL: "https://daytona-gang.firebaseio.com",
+  projectId: "daytona-gang",
+  storageBucket: "daytona-gang.appspot.com",
+  messagingSenderId: "263582169379",
+  appId: "1:263582169379:web:19d897dc6d07127ef8b496",
+  measurementId: "G-TP9Y25LP53",
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
+
+    const database = firebase.database();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
