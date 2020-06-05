2f941e4af50c7b95f81bbc022b166c75330ef784
Jordan Rhodes <jsrhodes15@gmail.com>
2020-06-05 16:27:04 -0400

    Use updater from App.js to update user in local state

--- src/components/SignIn/signin.js
+++ src/components/SignIn/signin.js
@@ -1,89 +1,81 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { compose } from "recompose";
+import React, {Component} from "react";
 import "./signin.css";
 
-import { SignUpLink } from "../SignUp/signup";
-import { withFirebase } from "../Firebase";
+import {SignUpLink} from "../SignUp/signup";
+import {withFirebase} from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
-const SignIn = () => (
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm />
-    <SignUpLink />
-  </div>
-);
-
 const INITIAL_STATE = {
-  email: "",
-  password: "",
-  user: null,
-  error: null,
+    email: "",
+    password: "",
+    user: null,
+    error: null,
 };
 
-class SignInFormBase extends Component {
-  constructor(props) {
-    super(props);
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
 
-    this.state = { ...INITIAL_STATE };
-  }
+        this.state = {...INITIAL_STATE};
+    }
 
-  onSubmit = (event) => {
-    const { email, password } = this.state;
+    onSubmit = (event) => {
+        event.preventDefault();
 
-    this.props.firebase
-      .doSignInWithEmailAndPassword(email, password)
-      .then((user) => {
-        this.setState({ ...INITIAL_STATE, user });
-        // console.log("this is user from signin: ", this.state);
+        const {email, password} = this.state;
+        const {updateUser, history} = this.props;
 
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch((error) => {
-        this.setState({ error });
-      });
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then((user) => {
+                updateUser({user});
+                history.push(ROUTES.HOME);
+            })
+            .catch((error) => {
+                updateUser({error});
+            });
 
-    event.preventDefault();
-  };
 
-  onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+    };
 
-  render() {
-    const { email, password, error } = this.state;
+    onChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    };
 
-    const isInvalid = password === "" || email === "";
+    render() {
+        const {email, password, error} = this.state;
 
-    return (
-      <form onSubmit={this.onSubmit} className="form">
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
+        const isInvalid = password === "" || email === "";
 
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+        return (
+            <div>
+                <h1>SignIn</h1>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input
+                        name="email"
+                        value={email}
+                        onChange={this.onChange}
+                        type="text"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        name="password"
+                        value={password}
+                        onChange={this.onChange}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button disabled={isInvalid} type="submit">
+                        Sign In
+                    </button>
+
+                    {error && <p>{error.message}</p>}
+                </form>
+                <SignUpLink/>
+            </div>
+        );
+    }
 }
 
-const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+export default withFirebase(SignIn);
 
-export default SignIn;
-
-export { SignInForm };
